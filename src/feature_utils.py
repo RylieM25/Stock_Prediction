9d7cd8222990a12be58ef2ae6dc9b735f0a6f482
RylieM25 <r.p.mayo@tcu.edu>
2026-04-07 16:40:10 -0500

    Update feature_utils.py

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -101,8 +101,6 @@ def convert_input_pca_regression(request_body, request_content_type):
 
     if option == 2:
 
-       if option == 2:
-
     X = FeatureEngineer(windows=[5,10,15,20,30,45,60]).transform(dataset[[target]])
     
     techIndicator_1 = 'RSI_15'
